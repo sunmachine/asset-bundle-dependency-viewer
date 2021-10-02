edac2193e1ef2ca5789ededa09dd0a44ae025220
Dan Peavey <danpeavey@gmail.com>
2021-10-02 12:02:42 -0700

    Deployment: First push to GitHub Pages.

--- vue.config.js
+++ vue.config.js
@@ -11,4 +11,10 @@ module.exports = {
             .use('vue-svg-loader')
             .loader('vue-svg-loader');
     },
+    outputDir: process.env.NODE_ENV === 'production'
+        ? 'docs' // GitHub Pages output dir.
+        : 'dist', // Default development.
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/asset-bundle-dependency-viewer/'
+        : '/'
 };
