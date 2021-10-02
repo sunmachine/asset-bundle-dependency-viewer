d86d5415b941275ad0050d3341b9ac6064be7dd7
Dan Peavey <danpeavey@gmail.com>
2021-10-02 11:54:40 -0700

    Project setup.

---
+++ src/components/BundleGraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="graph">
+    <h1>Asset Bundle Dependency Graph</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BundleGraph',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+.graph {
+
+}
+</style>
